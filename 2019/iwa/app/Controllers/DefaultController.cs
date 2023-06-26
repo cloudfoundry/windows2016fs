010abe8a3b11b9d25e6aedaa293d23e8d6b639e5
Amin Jamali <ajamali@vmware.com>
2023-06-26 17:13:44 +0000

    remove authorize

--- 2019/iwa/app/Controllers/DefaultController.cs
+++ 2019/iwa/app/Controllers/DefaultController.cs
@@ -26,12 +26,13 @@ namespace WindowsAuth.Controllers
         }
         [Route("~/sql")]
         [HttpGet]
-        [Authorize]
+        //[Authorize]
         public IHttpActionResult Sql()
         {
             
             var identity = Thread.CurrentPrincipal.Identity;
             var cs = System.Configuration.ConfigurationManager.ConnectionStrings["mydb"].ConnectionString;
+            Console.WriteLine(cs);
             string queryString = "SELECT * FROM MyTest;";
             using (SqlConnection connection = new SqlConnection(cs))
             {
@@ -51,6 +52,39 @@ namespace WindowsAuth.Controllers
                         // Always call Close when done reading.
     reader.Close();
                     }
+            }
+
+            return Ok("Failed to read");
+        }
+
+        [Route("~/sql-login")]
+        [HttpGet]
+        //[Authorize]
+        public IHttpActionResult SqlLogin()
+        {
+
+            var identity = Thread.CurrentPrincipal.Identity;
+            var cs = System.Configuration.ConfigurationManager.ConnectionStrings["mydb-sql-login"].ConnectionString;
+            Console.WriteLine(cs);
+            string queryString = "SELECT * FROM MyTest;";
+            using (SqlConnection connection = new SqlConnection(cs))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(String.Format("{0}, {1}", reader["Name"], reader["Age"]));
+                        return Ok(String.Format("{0}, {1}", reader["Name"], reader["Age"]));
+                    }
+                }
+                finally
+                {
+                    // Always call Close when done reading.
+                    reader.Close();
+                }
             }
 
             return Ok("Failed to read");
