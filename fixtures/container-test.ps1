8ed148e53b0ce5b13cc896ba54934ed5766e1c94
Amelia Downs <amelia.downs@broadcom.com>
2024-09-06 11:11:11 +0000

    add a connectaddress and listenaddress
    Signed-off-by: Amin Jamali <amin.jamali@broadcom.com>

--- fixtures/container-test.ps1
+++ fixtures/container-test.ps1
@@ -2,8 +2,7 @@ $ErrorActionPreference = "Stop";
 trap { 
     $host.SetShouldExit(1) 
 }
-netsh interface portproxy add v4tov4 listenport=445 connectport=1025
-
+netsh interface portproxy add v4tov4 listenport=445 connectaddress=tcp.lend-2507159.z8cb02834.shepherd.lease connectport=1025 listenaddress=tcp.lend-2507159.z8cb02834.shepherd.lease
 net use t: $env:SHARE_UNC $env:SHARE_PASSWORD /user:$env:SHARE_USERNAME
 if ($LASTEXITCODE -ne 0) {
     echo "ERROR: could not create smb mapping"
